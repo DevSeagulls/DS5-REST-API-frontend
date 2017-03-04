37efa08fbc3ea91e2ae46d7b9b8a1ecbf0d8b429
CaspianSeagull <caspian.seagull@gmail.com>
2017-03-04 12:40:34 +0300

    Загрузка, создание и удаление актеров

---
+++ javascript/1/script.js
@@ -0,0 +1,105 @@
+function getActors() {
+
+	var XHR = new XMLHttpRequest();
+
+	XHR.open('GET', API('/actors/'), true);
+
+	XHR.send();
+
+	XHR.onload = function() {
+		var actors = JSON.parse(XHR.responseText);
+
+		showActors(actors);
+	}
+}
+
+
+function uploadActor(actorData) {
+	
+	var XHR = new XMLHttpRequest();
+
+	XHR.open('POST', API('/actors/'), true);
+
+	XHR.send(actorData);
+
+	XHR.onload = function() {
+		getActors();
+	}
+
+}
+
+
+function deleteActor(id) {
+	var XHR = new XMLHttpRequest();
+
+	XHR.open('DELETE', API('/actors/' + id + '/'), true);
+
+	XHR.send();
+
+	XHR.onload = function() {
+		getActors();
+	}
+}
+
+
+
+function showActors(actors) {
+	var HTML = '';
+
+	actors.forEach(function(actor, index) {
+		HTML += actorTemplate(actor);
+	});
+
+	var actorsContainer = document.querySelector('.actors');
+
+	actorsContainer.innerHTML = HTML;
+
+	initListeners();
+}
+
+function actorTemplate(actor) {
+	return `
+	<div class="actor">
+		<div class="actor__photo">
+			<img src="${actor.photo || '/'}" alt="">
+		</div>
+		<div class="actor__info">
+			<span>${actor.firstname} ${actor.lastname}</span>
+			<span>${actor.birthdate}</span>
+
+			<button class="delete-actor" data-id="${actor.id}">Удалить</button>
+		</div>
+	</div>
+	<hr>`;
+}
+
+function API(query) {
+	var api_domain = 'http://163.172.130.181:8000/api/v1';
+	return api_domain + query;
+}
+
+function initListeners() {
+	var delButtons = document.querySelectorAll('.delete-actor');
+
+	delButtons.forEach( function(button, i) {
+		button.addEventListener('click', (e) => {
+			let id = e.target.dataset.id;
+
+			deleteActor(id);
+		})
+	});
+}
+
+// Init
+getActors();
+
+
+document.querySelector("#add-actor").addEventListener('click', (e) => {
+	e.preventDefault();
+
+	var actorData = new FormData(document.forms.newActor);
+
+	uploadActor(actorData);
+
+	document.forms.newActor.reset();
+})
